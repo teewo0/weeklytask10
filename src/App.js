25d85b84f216b0a3bdd3564a5248a5192241e4a5
teewo0 <teewodavie@gmail.com>
2021-08-07 11:51:23 +0100

    updated weeklytask10

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import {
-  BrowserRouter as Router,
-  Switch,
   Route,
-  Link,
   NavLink
 } from "react-router-dom";
 import Home from './pages/Home';
